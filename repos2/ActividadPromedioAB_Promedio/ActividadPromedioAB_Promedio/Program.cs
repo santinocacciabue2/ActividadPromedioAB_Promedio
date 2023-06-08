6e3476c3192e3704d306635b2a0530ba7c02dc55
santinocacciabue2 <126773155+santinocacciabue2@users.noreply.github.com>
2023-06-08 20:11:11 -0300

    Add files via upload

---
+++ repos2/ActividadPromedioAB_Promedio/ActividadPromedioAB_Promedio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadPromedioAB_Promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            Console.WriteLine("Seleccione una opcion\n 1- Agregar valor\n 2-Ver promedio\n 3-Salir");
+            opcion = Convert.ToInt32(Console.ReadLine());
+            Promedio promedio = new Promedio();
+            do
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Ingrese un valor");
+                            double valor = Convert.ToDouble(Console.ReadLine());
+                            promedio.AgregarValor(valor);
+                        }
+                        break;
+                    case 2:
+                        {
+                            promedio.CalcularPromedio();
+                            double prom = promedio.CalcularPromedio();
+                            Console.WriteLine("Promedio: {0}", prom);
+                        }
+                        break;
+                }
+                Console.WriteLine("Seleccione una opcion\n 1- Agregar valor\n 2-Ver promedio\n 3-Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+            } while (opcion != 3);
+
+        }
+    }
+}
---
+++ repos2/ActividadPromedioAB_Promedio/ActividadPromedioAB_Promedio/Promedio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadPromedioAB_Promedio
+{
+    internal class Promedio
+    {
+        private double total;
+        private double valor;
+        private int contador;
+        public void AgregarValor(double valor)
+        {
+            total += valor;
+            contador++;
+        }
+        public double CalcularPromedio()
+        {
+            double prom;
+            prom = total/contador;
+            return prom;
+        }
+    }
+}
